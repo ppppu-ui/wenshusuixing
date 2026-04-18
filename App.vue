2258caba1b8acfe67f9d2217fc1b9a4d50e48edd
qin peng <192948767@qq.com>
2026-04-18 17:53:20 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 页面基础样式 */
+	page {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Helvetica Neue', Arial, sans-serif;
+		color: #2C3E50;
+		background-color: #F8FAF9;
+	}
+
+</style>
